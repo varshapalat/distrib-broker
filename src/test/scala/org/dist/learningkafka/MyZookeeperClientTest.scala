337ac682f2a6e5e0051aade0e2f9885e2c3a76fb
Varsha Palat <varsha.p.aux@go-jek.com>
2020-07-14 23:16:48 +0530

    Master node subscribing to broker change listener - registering and destroying brokers

--- src/test/scala/org/dist/learningkafka/MyZookeeperClientTest.scala
+++ src/test/scala/org/dist/learningkafka/MyZookeeperClientTest.scala
@@ -1,6 +1,8 @@
 package org.dist.learningkafka
 
-import org.dist.common.ZookeeperTestHarness
+import org.I0Itec.zkclient.ZkClient
+import org.dist.common.{TestUtils, ZookeeperTestHarness}
+import org.dist.simplekafka.util.ZKStringSerializer
 import org.dist.simplekafka.util.ZkUtils.Broker
 
 class MyZookeeperClientTest extends ZookeeperTestHarness {
@@ -13,5 +15,37 @@ class MyZookeeperClientTest extends ZookeeperTestHarness {
 
     assert(3 == myZookeeperClient.getAllBrokers().size)
     assertResult(Broker(0, "10.10.10.10", 8000))( myZookeeperClient.getBrokerInfo(0))
+  }
+
+  test("master node subscribes and listens if any of the brokers fails") {
+    val zkClient1 = new ZkClient(zkConnect, zkSessionTimeout, zkConnectionTimeout, ZKStringSerializer)
+    val myZookeeperClient1 = new MyZookeeperClient(zkClient1)
+
+    val zkClient2 = new ZkClient(zkConnect, zkSessionTimeout, zkConnectionTimeout, ZKStringSerializer)
+    val myZookeeperClient2 = new MyZookeeperClient(zkClient2)
+
+    val zkClient3 = new ZkClient(zkConnect, zkSessionTimeout, zkConnectionTimeout, ZKStringSerializer)
+    val myZookeeperClient3 = new MyZookeeperClient(zkClient3)
+
+    val listener = new MyBrokerChangeListener(myZookeeperClient1)
+    myZookeeperClient1.subscribeBrokerChangeListener(listener)
+
+    myZookeeperClient1.registerBroker(Broker(0, "10.10.10.10", 8000))
+    myZookeeperClient2.registerBroker(Broker(1, "10.10.10.11", 8000))
+    myZookeeperClient3.registerBroker(Broker(2, "10.10.10.12", 8000))
+
+    TestUtils.waitUntilTrue(() => {
+      listener.liveBrokers.size == 3
+    }, "Waiting for all brokers to get added", 1000)
+
+    zkClient2.close()
+    TestUtils.waitUntilTrue(() => {
+      listener.liveBrokers.size == 2
+    }, "Waiting for all brokers to get added", 1000)
+
+    zkClient3.close()
+    TestUtils.waitUntilTrue(() => {
+      listener.liveBrokers.size == 1
+    }, "Waiting for all brokers to get added", 1000)
   }
 }
